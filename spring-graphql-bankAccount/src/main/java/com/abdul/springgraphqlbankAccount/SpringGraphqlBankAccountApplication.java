00bd29e77200dfceb7dabaf0682a82e5899fe8d8
Abdul-Barambu <abdulkadirbarambu1@gmail.com>
2024-04-02 14:55:13 +0100

    first commit

---
+++ spring-graphql-bankAccount/src/main/java/com/abdul/springgraphqlbankAccount/SpringGraphqlBankAccountApplication.java
@@ -0,0 +1,13 @@
+package com.abdul.springgraphqlbankAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlBankAccountApplication.class, args);
+	}
+
+}
